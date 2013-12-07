0c038c388a2e3883b17c83703a825f3fe555dc04
Grant Gingell <grantgingell@gmail.com>
2013-12-07 16:02:54 -0500

    Added static folder skeleton and stylesheet.

--- hello.py
+++ hello.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
