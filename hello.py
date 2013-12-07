0cd99291708fcfdf24b2b928615537b9a6a0cd5e
Grant Gingell <grantgingell@gmail.com>
2013-12-07 18:34:45 -0500

    Added templates folder and instance config settings.

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 __author__ = 'grantgingell'
 
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, instance_path='/Users/grantgingell/Documents', instance_relative_config=True)
+#db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
@@ -11,8 +15,16 @@ def hello_world():
 def show_user_profile(username):
     return 'User %s' % username
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name = None):
+    return render_template('hello.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
-    app.debug = True
+
+    app.config.from_pyfile('flask-quickstart-dev.cfg', silent=False)
     app.run()
