62ff455d9ea7d9d292405727c57a2c28a492115a
Grant Gingell <grantgingell@gmail.com>
2013-12-07 15:41:38 -0500

    Entered debug mode.

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 __author__ = 'grantgingell'
 
-<<<<<<< HEAD
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,6 +8,5 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
-=======
->>>>>>> 6b3c3bf623352ff91ff05742f309e549f6b89e87
